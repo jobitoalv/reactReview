84405c1de95e9c9cb2d4ccbf997a4a9b4d2d6fd3
Job Alvarez <jobitoalv@gmail.com>
2023-01-09 10:05:11 +1000

    added different classes

--- src/Review.js
+++ src/Review.js
@@ -9,9 +9,12 @@ const Review = () => {
     <div className='img-container'>
       <img src={image} alt={name} className='person-img'/>
       <span className='quote-icon'>
-        <FaQuoteRight/>
+        <FaQuoteRight />
       </span>
     </div>
+    <h4 className='author'>{name}</h4>
+    <p className='job'>{job}</p>
+    <p className='info'>{text}</p>
   </article>;
 };
 
